8393dcc2c0a48dcc027b118c3a50846fc0a1a482
Misko183 <rtmike183@gmail.com>
2023-01-12 02:15:31 +0100

    Edited admin navigation

--- admin/template/nav.php
+++ admin/template/nav.php
@@ -23,7 +23,7 @@
                                 <?php 
                                 $user = $_SESSION["name"];
                                 ?>    
-                                <?php echo $user; ?></span></a>
+                                <?php echo $user; ?></span><i class="fas fa-bars"></i></a>
                                     <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in"><a class="dropdown-item" href="#"><i class="fas fa-user fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Profile</a><a class="dropdown-item" href="#"><i class="fas fa-cogs fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Settings</a><a class="dropdown-item" href="#"><i class="fas fa-list fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Activity log</a>
                                         <div class="dropdown-divider"></div><a class="dropdown-item" href="../scripts/logout.php?continue=<?php echo $link; ?>"><i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Logout</a>
                                     </div>
